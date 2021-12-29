9c1fb32fc271f0b6398c5fc3d7e76dfaf073285e
jackanderson12 <43770968+jackanderson12@users.noreply.github.com>
2021-12-29 15:46:26 -0500

    Quiz Array Added
    Added the quiz array

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -14,12 +14,24 @@ class ViewController: UIViewController {
     @IBOutlet weak var falseButton: UIButton!
     @IBOutlet weak var progressBar: UIProgressView!
     
+    let quiz = [
+        "Four + Two is equal to Six.",
+        "Five - Three is greater than One",
+        "Three + Eight is less than Ten"
+    ]
+    
+    var questionNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        questionLabel.text = "Four + Two is equal to Six"
+        updateUI()
     }
     @IBAction func answerButtonPressed(_ sender: UIButton) {
-        
+        questionNumber += 1
+        updateUI()
+    }
+    func updateUI() {
+        questionLabel.text = quiz[questionNumber]
     }
     
 
